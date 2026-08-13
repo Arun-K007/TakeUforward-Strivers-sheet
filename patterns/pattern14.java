3d928c6e55c0b069f740a6d76eacecdb09e8deda
Arun-K007 <arunkrishnamoorthy324@gmail.com>
2026-08-13 14:19:59 +0530

    Create pattern14.java

---
+++ patterns/pattern14.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void pattern14(int n) {
+        	    
+	    for(int row=1;row<=n;row++){
+	        char count='A';
+	        for(int i=1;i<=row;i++){
+	      System.out.print(count);
+	      count++;
+	        }
+	        System.out.println();
+	    }
+
+    }
+}
