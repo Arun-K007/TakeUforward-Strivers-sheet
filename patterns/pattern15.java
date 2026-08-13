1c90cfa63e63fd2093a1b2e0e86bcdafb77ba367
Arun-K007 <arunkrishnamoorthy324@gmail.com>
2026-08-13 14:22:37 +0530

    Create pattern15.java

---
+++ patterns/pattern15.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void pattern15(int n) {
+        	    for(int row=n;row>0;row--){
+	        char count='A';
+	        for(int i=1;i<=row;i++){
+	      System.out.print(count);
+	      count++;
+	        }
+	        System.out.println();
+	    }
+
+    }
+}
