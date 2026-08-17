e4037d5e07cb7c3a2b5c410604e89383c56488c0
Arun-K007 <arunkrishnamoorthy324@gmail.com>
2026-08-17 14:45:37 +0530

    Create pattern17.java

---
+++ patterns/pattern17.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public void pattern17(int n) {
+        	for(int row=1;row<=n;row++){
+		    for(int i=1;i<=n-row;i++){
+		        System.out.print(" ");
+		    }
+		    for(int i=1;i<=row;i++){
+		        
+		        System.out.print((char)(i+64));
+		    }
+		    
+		    for(int i=row-1;i>0;i--){
+		        System.out.print((char)(i+64));
+		    }
+		    System.out.println();
+	}
+
+    }
+}
